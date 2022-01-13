8f70ddcd019018728d2dac7237463edb542e7959
jayl2sw <jayl2sf@gmail.com>
2022-01-13 14:53:34 +0900

    v1.0.1

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -12,7 +12,6 @@ today = datetime.today().strftime('%Y-%m-%d')
 drt = ()
 windows_user_name = os.path.expanduser('~')
 
-
 def get_price(code):
     # DATA를 불러오는 부분 입니다.
     url = 'http://finance.daum.net/api/charts/A%s/days?limit=%d&adjusted=true' % (code, 90)
@@ -30,6 +29,27 @@ def get_price(code):
     headers['Referer'] = 'http://finance.daum.net/quotes/A%s' % code
     r = requests.get(url, headers=headers)
 
+    url_1000 = 'http://finance.daum.net/api/charts/A%s/days?limit=%d&adjusted=true' % (code, 1000)
+    headers_1000 = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Cookie': 'GS_font_Name_no=0; GS_font_size=16; _ga=GA1.3.937989519.1493034297; webid=bb619e03ecbf4672b8d38a3fcedc3f8c; _ga=GA1.2.937989519.1493034297; _gid=GA1.2.215330840.1541556419; KAKAO_STOCK_RECENT=[%22A069500%22]; recentMenus=[{%22destination%22:%22chart%22%2C%22title%22:%22%EC%B0%A8%ED%8A%B8%22}%2C{%22destination%22:%22current%22%2C%22title%22:%22%ED%98%84%EC%9E%AC%EA%B0%80%22}]; TIARA=C-Tax5zAJ3L1CwQFDxYNxe-9yt4xuvAcw3IjfDg6hlCbJ_KXLZZhwEPhrMuSc5Rv1oty5obaYZzBQS5Du9ne5x7XZds-vHVF; webid_sync=1541565778037; _gat_gtag_UA_128578811_1=1; _dfs=VFlXMkVwUGJENlVvc1B3V2NaV1pFdHhpNTVZdnRZTWFZQWZwTzBPYWRxMFNVL3VrODRLY1VlbXI0dHhBZlJzcE03SS9Vblh0U2p2L2V2b3hQbU5mNlE9PS0tcGI2aXQrZ21qY0hFbzJ0S1hkaEhrZz09--6eba3111e6ac36d893bbc58439d2a3e0304c7cf3',
+        'Host': 'finance.daum.net',
+        'If-None-Match': 'W/"23501689faaaf24452ece4a039a904fd"',
+        'Referer': 'http://finance.daum.net/quotes/A069500',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    }
+    headers_1000['Referer'] = 'http://finace.daum.et/quotes/A%s' % code
+    r_1000 = requests.get(url_1000, headers=headers_1000)
+
+    data_1000 = json.loads(r_1000.text)
+    df_1000 = pd.DataFrame(data_1000['data'])
+    s_highest_1000 = df_1000["highPrice"].max()
+    s_lowest_1000 = df_1000["lowPrice"].min()
+
+
     # DATA를 보기 좋게 편집하는 부분 입니다.
     data = json.loads(r.text)
     df = pd.DataFrame(data['data'])
@@ -38,15 +58,16 @@ def get_price(code):
     s_price = df.iat[-1, 3]
     s_highest = df["highPrice"].max()
     s_lowest = df["lowPrice"].min()
+    s_percentile = "{:.2f}".format((s_price - s_lowest)/(s_highest - s_lowest))
+    s_percentile_1000 = "{:.2f}".format((s_price - s_lowest_1000) / (s_highest_1000 - s_lowest_1000))
 
-    df1 = pd.DataFrame({"Code": [s_code], "Price": [s_price], "Highest_90": [s_highest], "Lowest_90": [s_lowest]})
-    print(df1)
+    df1 = pd.DataFrame({"Code": [s_code], "Price": [s_price], "90일_H": [s_highest], "90_L": [s_lowest],
+                        "90_P": [s_percentile], "1000일_H": [s_highest_1000], "1000일_L": [s_lowest_1000],
+                        "1000일_P": [s_percentile_1000]})
     return df1
-
 
 def get_info():
     global drt
-    print(drt)
     df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
 
     df = df[['회사명', '종목코드']]
@@ -67,8 +88,10 @@ def get_info():
         else:
             nc_df = df.loc[df.index[df['Name'] == in_name]]
             fnc_df = fnc_df.append(nc_df)
-            print(fnc_df)
             i = i + 1
+
+
+
 
     j = 0
     while j < len(fnc_df):
@@ -87,7 +110,6 @@ def get_info():
 
     f = pd.concat([fnc_df, fcphl_df], axis=1)
     final = f.drop(['index', 'Code'], axis=1)
-    print(final)
 
     global windows_user_name
     global today
@@ -99,6 +121,8 @@ class MainWindow(QMainWindow):
         btn = self.sender()
         btn.setText('runing')
         btn.setDisabled(True)  # 버튼 비활성화
+
+
 
     def filedialog_open(self):
 
@@ -112,6 +136,7 @@ class MainWindow(QMainWindow):
             self.path.setText(drt)
             print('filepath : ', drt)
             print('filesort : ', fname[1])
+
         else:
             QMessageBox.about(self, 'Warning', '파일을 선택하지 않았습니다.')
 
@@ -119,8 +144,8 @@ class MainWindow(QMainWindow):
         global drt
         super().__init__()
         # 윈도우 설정
-        self.setGeometry(1500, 200, 800, 120)  # x, y, w, h
-        self.setWindowTitle('주식분석기 ver1')
+        self.setGeometry(300, 200, 800, 120)  # x, y, w, h
+        self.setWindowTitle('아빠 화이팅!')
 
         self.Lbl1 = QLabel(self)
         self.Lbl1.setText('주식 종목 파일:')
@@ -136,14 +161,13 @@ class MainWindow(QMainWindow):
         self.path = QTextEdit(self)
         self.path.setGeometry(165, 20, 400, 74)
 
-        self.exe_btn = QPushButton('확인', self)
+        self.exe_btn = QPushButton('확인',self)
         self.exe_btn.clicked.connect(get_info)
         self.exe_btn.setGeometry(580, 60, 95, 32)
 
         self.quit_btn = QPushButton('취소', self)
         self.quit_btn.setGeometry(685, 60, 95, 32)
         self.quit_btn.clicked.connect(QApplication.instance().quit)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
