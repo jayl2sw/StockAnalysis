364e628535af463f4fe09cd10fb579d44684f9d5
jayl2sw <jayl2sf@gmail.com>
2022-01-13 14:28:30 +0900

    v1.0.0

---
+++ StockAnalysis.py
@@ -0,0 +1,152 @@
+import json
+import openpyxl
+import pandas as pd
+import requests
+from dateutil.utils import today
+from datetime import datetime
+import os
+import sys
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication, QPushButton, QMessageBox, QLabel, QTextEdit
+
+today = datetime.today().strftime('%Y-%m-%d')
+drt = ()
+windows_user_name = os.path.expanduser('~')
+
+
+def get_price(code):
+    # DATA를 불러오는 부분 입니다.
+    url = 'http://finance.daum.net/api/charts/A%s/days?limit=%d&adjusted=true' % (code, 90)
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Cookie': 'GS_font_Name_no=0; GS_font_size=16; _ga=GA1.3.937989519.1493034297; webid=bb619e03ecbf4672b8d38a3fcedc3f8c; _ga=GA1.2.937989519.1493034297; _gid=GA1.2.215330840.1541556419; KAKAO_STOCK_RECENT=[%22A069500%22]; recentMenus=[{%22destination%22:%22chart%22%2C%22title%22:%22%EC%B0%A8%ED%8A%B8%22}%2C{%22destination%22:%22current%22%2C%22title%22:%22%ED%98%84%EC%9E%AC%EA%B0%80%22}]; TIARA=C-Tax5zAJ3L1CwQFDxYNxe-9yt4xuvAcw3IjfDg6hlCbJ_KXLZZhwEPhrMuSc5Rv1oty5obaYZzBQS5Du9ne5x7XZds-vHVF; webid_sync=1541565778037; _gat_gtag_UA_128578811_1=1; _dfs=VFlXMkVwUGJENlVvc1B3V2NaV1pFdHhpNTVZdnRZTWFZQWZwTzBPYWRxMFNVL3VrODRLY1VlbXI0dHhBZlJzcE03SS9Vblh0U2p2L2V2b3hQbU5mNlE9PS0tcGI2aXQrZ21qY0hFbzJ0S1hkaEhrZz09--6eba3111e6ac36d893bbc58439d2a3e0304c7cf3',
+        'Host': 'finance.daum.net',
+        'If-None-Match': 'W/"23501689faaaf24452ece4a039a904fd"',
+        'Referer': 'http://finance.daum.net/quotes/A069500',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+    }
+    headers['Referer'] = 'http://finance.daum.net/quotes/A%s' % code
+    r = requests.get(url, headers=headers)
+
+    # DATA를 보기 좋게 편집하는 부분 입니다.
+    data = json.loads(r.text)
+    df = pd.DataFrame(data['data'])
+
+    s_code = df.iat[2, 0]
+    s_price = df.iat[-1, 3]
+    s_highest = df["highPrice"].max()
+    s_lowest = df["lowPrice"].min()
+
+    df1 = pd.DataFrame({"Code": [s_code], "Price": [s_price], "Highest_90": [s_highest], "Lowest_90": [s_lowest]})
+    print(df1)
+    return df1
+
+
+def get_info():
+    global drt
+    print(drt)
+    df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
+
+    df = df[['회사명', '종목코드']]
+    df = df.rename(columns={'회사명': 'Name', '종목코드': 'Code'})
+
+    if drt[-1] == 'x':
+        in_df = pd.read_excel(drt)
+    else:
+        in_df = pd.read_csv(drt)
+    i = 0
+
+    while i < len(in_df):
+        in_name = in_df.iat[i, 0]
+        if i == 0:
+            nc_df = df.loc[df.index[df['Name'] == in_name]]
+            fnc_df = nc_df
+            i = i + 1
+        else:
+            nc_df = df.loc[df.index[df['Name'] == in_name]]
+            fnc_df = fnc_df.append(nc_df)
+            print(fnc_df)
+            i = i + 1
+
+    j = 0
+    while j < len(fnc_df):
+        in_code = str(fnc_df.iat[j, 1]).zfill(6)
+        if j == 0:
+            cphl_df = get_price(in_code)
+            fcphl_df = cphl_df
+            j = j + 1
+        else:
+            cphl_df = get_price(in_code)
+            fcphl_df = fcphl_df.append(cphl_df)
+            j = j + 1
+
+    fnc_df = fnc_df.reset_index()
+    fcphl_df = fcphl_df.reset_index()
+
+    f = pd.concat([fnc_df, fcphl_df], axis=1)
+    final = f.drop(['index', 'Code'], axis=1)
+    print(final)
+
+    global windows_user_name
+    global today
+    final.to_excel('{}/Desktop/{}.xlsx'.format(windows_user_name, today))
+
+
+class MainWindow(QMainWindow):
+    def btnRun_clicked(self):
+        btn = self.sender()
+        btn.setText('runing')
+        btn.setDisabled(True)  # 버튼 비활성화
+
+    def filedialog_open(self):
+
+        fname = QFileDialog.getOpenFileName(self, 'Open File', '',
+                                            'All File(*);; html File(*.html *.htm)')
+        global drt
+        drt = str(fname[0])
+
+        if fname[0]:
+            # 튜플 데이터에서 첫 번째 인자 값이 주소이다.
+            self.path.setText(drt)
+            print('filepath : ', drt)
+            print('filesort : ', fname[1])
+        else:
+            QMessageBox.about(self, 'Warning', '파일을 선택하지 않았습니다.')
+
+    def __init__(self):
+        global drt
+        super().__init__()
+        # 윈도우 설정
+        self.setGeometry(1500, 200, 800, 120)  # x, y, w, h
+        self.setWindowTitle('주식분석기 ver1')
+
+        self.Lbl1 = QLabel(self)
+        self.Lbl1.setText('주식 종목 파일:')
+        self.Lbl1.setGeometry(20, 20, 180, 32)
+
+        # QButton 위젯 생성 - FileDialog 을 띄위기 위한 버튼
+        self.button = QPushButton('Search...', self)
+        self.button.clicked.connect(self.filedialog_open)
+        self.button.setGeometry(580, 20, 200, 32)
+
+        # QTextEdit 파일 읽은 내용 표시
+
+        self.path = QTextEdit(self)
+        self.path.setGeometry(165, 20, 400, 74)
+
+        self.exe_btn = QPushButton('확인', self)
+        self.exe_btn.clicked.connect(get_info)
+        self.exe_btn.setGeometry(580, 60, 95, 32)
+
+        self.quit_btn = QPushButton('취소', self)
+        self.quit_btn.setGeometry(685, 60, 95, 32)
+        self.quit_btn.clicked.connect(QApplication.instance().quit)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Stock Analysis
+
+#### v 1.0
+
+- Name으로부터 Code를 추출해냄
+- Code로 현재가, 90일간 최고가, 90일간 최저가 추출
